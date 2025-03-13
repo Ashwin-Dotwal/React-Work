4940f89b004c16d20ba35432047330aa23abfccb
Ashwin Dotwal <ashwindotwal09@gmail.com>
2025-03-13 21:55:46 +0530

    add usestate hook

---
+++ hookusestate/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import './counter.css'
+
+function App(){
+  return(
+    <div>
+ <counter/>
+    </div>
+  )
+}
+
+export default App
---
+++ hookusestate/src/components/1counter.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const counter = () => {
+  return (
+    <div className='counter-container'>
+      <p id='para'>you have clicked 0 times</p>
+      <button id='btn'>click me</button>
+      
+    </div>
+  )
+}
+
+export default counter
+
