027cbbb60ac157fb5225100a6b2dc9fc944aee07
Ashwin Dotwal <ashwindotwal09@gmail.com>
2025-03-24 10:47:26 +0530

    add some code

--- react-hook-form/src/App.jsx
+++ react-hook-form/src/App.jsx
@@ -1,13 +1,46 @@
-
-import './App.css'
+import "./App.css";
+import { useForm } from "react-hook-form";
 
 function App() {
-  
-  return (
-<div>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-</div>  
-)
+  function onSubmit(data){
+     console.log("submitting the form",data);
+  }
+  
+
+  return (
+   
+   <form onSubmit={handleSubmit(onSubmit)}>
+    
+
+    <div>
+    <label>FirstName:</label>
+    
+    <input {...register("firstName", { required: true,minLength:3 })}/>
+    </div>
+    <br />
+    <div>
+    <label>MiddleName:</label>
+    
+    <input  {...register("middleName", { required: true,minLength:3 })}/>
+    </div>
+    <br />
+    <div>
+    <label>LastName:</label>
+   
+    <input{...register("lastName ",{ required: true,minLength:3 })}/>
+    </div>
+
+    <br />
+    <input type='submit'/>
+   </form>
+  
+  );
 }
 
-export default App
+export default App;
