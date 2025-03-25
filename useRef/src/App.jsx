004ac6987884a9ba49e3bfe49ff3b3ec90d2b985
Ashwin Dotwal <ashwindotwal09@gmail.com>
2025-03-25 20:11:22 +0530

    add useRef

--- useRef/src/App.jsx
+++ useRef/src/App.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState,useRef } from 'react'
 import './App.css'
 
 function App() {
-  const[count,setCount]=useState(0);
+ {/* const[count,setCount]=useState(0);
   let val=useRef(0);
 
   let btnRef=useRef();
@@ -21,17 +21,51 @@ function App() {
   function changecolor(){
     btnRef.current.style.backgroundColor="red";
 
+  }*/}
+
+
+ 
+
+
+  const[time,setTime]=useState(0);
+
+  let timerRef=useRef(null);
+
+  function StartTimer(){
+    timerRef.current=setInterval(() => {
+      setTime(time=>time+1);
+      
+    }, 1000);
+
   }
-  
+  function stopTimer(){
+    clearInterval(timerRef.current);
+    timerRef.current=null;
+
+  }
+  function resetTimer(){
+    stopTimer();
+    setTime(0);
+
+  }
 
   return (
-   <div>
-    <button ref={btnRef} onClick={handleIncrement}>Increment</button>
+  <div>
+    <h1>stopWatch:{time}seconds</h1>
+    <button onClick={StartTimer}>Start</button>
+    <br /><br />
+    <button onClick={stopTimer}>Stop</button>
+    <br /><br />
+
+    <button onClick={resetTimer}>Reset</button>
+
+
+     {/*<button ref={btnRef} onClick={handleIncrement}>Increment</button>
     <br />
     <button onClick={changecolor}>change color 1st buttton</button>
     <div>
       count:{count}
-    </div>
+    </div>*/}
    </div>
   )
 }
