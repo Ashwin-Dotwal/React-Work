78bb0d477154c6085e089895a889f3e29ef24cd6
Ashwin Dotwal <ashwindotwal09@gmail.com>
2026-03-07 23:01:14 +0530

    add some code

---
+++ React2026March/01-folder/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div id="dad">
+        <h1 id="c1">helo</h1>
+        <h2 id="c2">hello</h2>
+      </div>
+
+      <div id="chcha">
+        <h1 id="b1">jii</h1>
+        <h2 id="b2">jii</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
